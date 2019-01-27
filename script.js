20ea0f72737543bc38c7a3d2f6be9963579fdcb3
AnaisAna <anais.fradin@laposte.net>
2019-01-27 21:24:28 +0100

    exercice3.html

---
+++ script.js
@@ -0,0 +1,11 @@
+//Déclaration de fonction
+function textgreen(){
+  //style.color='' est pour dire d'ajouter une couleur à l'écriture lorsque nous cliquons sur un bouton
+  document.getElementById('text').style.color='green';
+}
+function textred(){
+  document.getElementById('text').style.color='red';
+}
+function textblue(){
+  document.getElementById('text').style.color='blue';
+}
